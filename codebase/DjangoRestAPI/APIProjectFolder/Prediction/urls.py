4e4d26b308053381a8dba5add0aa7109beab31d1
Sayantan Chatterjee <44543262+Sayantan-Ch@users.noreply.github.com>
2022-10-23 20:35:45 +0530

    Delete Directory

--- codebase/DjangoRestAPI/APIProjectFolder/Prediction/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-import Prediction.views as views
-
-urlpatterns = [
-    path('predict/', views.Disease_Predict.as_view(), name = 'predict'),
-    path('secondAdvice/', views.secondAdvice_page),
-    path('abc/', views.check_advice),
-    path('doctorPage/<str:disease>/', views.recommendDoctor),
-]
